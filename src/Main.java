ceba25e00153488ecb687f0316e42cbc7af30d9f
Ondrej Drtina <drtinao@office365.zcu.cz>
2020-12-27 14:51:35 +0100

    Created POC of information retrieving from created instances

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 
 
 public class Main {
-    static String testStr = "if(i == 4){c = 5; d = 4;} newValue = (i == 4) ? 10 : 20; int d = 4; bool test[3]; const string s = \"ahoj\"; cislo = porno;";
+    static String testStr = "if(i == 4){int c = 5; string s = \"ahoj\"; bool tr = true;} newValue = (i == 4) ? 10 : 20; if(r == 8){c = 4;}int d = 4; bool test[3]; const string s = \"ahoj\"; cislo = porno;";
 
     public static void main(String[] args){
         //System.out.println("jajajja");
@@ -27,9 +27,26 @@ public class Main {
         ArrayList<Istatement> encounteredStatements = varAss.getEncounteredStatements();
         System.out.println("Got number of statements: " + encounteredStatements.size());
         HashMap<Istatement, EallStatementType> statements = parseStatements(encounteredStatements);
-//        String name = parser.start().children.get(0).getText();
-//
-//        System.out.println(name);
+
+        //got statement list, ok -> parse multiline statements and get its content
+        for(int i = 0; i < encounteredStatements.size(); i++){
+            Istatement statement = encounteredStatements.get(i); //get one statement
+
+            if(statement instanceof ImultiLineStatement){ //statement is multiline, retrieve its content
+                ImultiLineStatement multiStatement = (ImultiLineStatement) statement; //cast to multiline
+                ArrayList<Istatement> innerStatements = multiStatement.getInnerStatement(); //inner statements included in multiline statement
+
+                System.out.println("Inside: " +statement + " - START");
+
+                HashMap<Istatement, EallStatementType> innerStatementsMap = parseInnerMultiStatement((ImultiLineStatement) statement);
+
+                for(int j = 0; j < innerStatements.size(); j++){
+                    System.out.println("Got: " + innerStatementsMap.get(innerStatements.get(j)));
+                }
+
+                System.out.println("Inside: " +statement + " - END");
+            }
+        }
     }
 
     /**
@@ -100,10 +117,79 @@ public class Main {
         for (Istatement name: statementTypeMap.keySet()){
             String key = name.toString();
             String value = statementTypeMap.get(name).toString();
-            System.out.println("Got: " + key + " " + value);
+            //System.out.println("Got: " + key + " " + value);
         }
 
-        System.out.println("Then: " + statementTypeMap.get(encounteredStatements.get(0)));
+        //System.out.println("Then: " + statementTypeMap.get(encounteredStatements.get(0)));
+
+        return statementTypeMap;
+    }
+
+    /**
+     * Takes multiline statement reference and returns HashMap with its content.
+     * @param multiLineStatement instance of class which implements ImultiLineStatement
+     * @return HashMap in which key = instance of class which implements Istatement ; value = value from EallStatementType which tells the actual type of key
+     */
+    private static HashMap<Istatement, EallStatementType> parseInnerMultiStatement(ImultiLineStatement multiLineStatement){
+        HashMap<Istatement, EallStatementType> statementTypeMap = new HashMap<Istatement, EallStatementType>();
+
+        ArrayList<Istatement> innerStatements = multiLineStatement.getInnerStatement();
+
+        for(int i = 0; i < innerStatements.size(); i++){ //go through inner statements
+            Istatement statement = innerStatements.get(i); //get one inner statement
+
+            if(statement instanceof IoneLineStatement){ //kind of oneline statement
+                if(statement instanceof arrBoolAssign){
+                    statementTypeMap.put(statement, EallStatementType.ARR_BOOL_ASSIGN);
+                }else if(statement instanceof arrBoolDeclaration){
+                    statementTypeMap.put(statement, EallStatementType.ARR_BOOL_DECLARATION);
+                }else if(statement instanceof arrIntAssign){
+                    statementTypeMap.put(statement, EallStatementType.ARR_INT_ASSIGN);
+                }else if(statement instanceof arrIntDeclaration){
+                    statementTypeMap.put(statement, EallStatementType.ARR_INT_DECLARATION);
+                }else if(statement instanceof boolAssign){
+                    statementTypeMap.put(statement, EallStatementType.BOOL_ASSIGN);
+                }else if(statement instanceof boolDeclaration){
+                    statementTypeMap.put(statement, EallStatementType.BOOL_DECLARATION);
+                }else if(statement instanceof constBoolDeclaration){
+                    statementTypeMap.put(statement, EallStatementType.CONST_BOOL_DECLARATION);
+                }else if(statement instanceof constIntDeclaration){
+                    statementTypeMap.put(statement, EallStatementType.CONST_INT_DECLARATION);
+                }else if(statement instanceof constStringDeclaration){
+                    statementTypeMap.put(statement, EallStatementType.CONST_STRING_DECLARATION);
+                }else if(statement instanceof intAssign){
+                    statementTypeMap.put(statement, EallStatementType.INT_ASSIGN);
+                }else if(statement instanceof intDeclaration){
+                    statementTypeMap.put(statement, EallStatementType.INT_DECLARATION);
+                }else if(statement instanceof procedureCall){
+                    statementTypeMap.put(statement, EallStatementType.PROCEDURE_CALL);
+                }else if(statement instanceof stringAssign){
+                    statementTypeMap.put(statement, EallStatementType.STRING_ASSIGN);
+                }else if(statement instanceof stringDeclaration){
+                    statementTypeMap.put(statement, EallStatementType.STRING_DECLARATION);
+                }else if(statement instanceof unknownArrAssign){
+                    statementTypeMap.put(statement, EallStatementType.UNKNOWN_ARR_ASSIGN);
+                }else if(statement instanceof unknownAssign){
+                    statementTypeMap.put(statement, EallStatementType.UNKNOWN_ASSIGN);
+                }
+            }else if(statement instanceof ImultiLineStatement){ //kind of multiline statement
+                if(statement instanceof doWhileCycle){
+                    statementTypeMap.put(statement, EallStatementType.DO_WHILE_CYCLE);
+                }else if(statement instanceof forCycle){
+                    statementTypeMap.put(statement, EallStatementType.FOR_CYCLE);
+                }else if(statement instanceof foreachCycle){
+                    statementTypeMap.put(statement, EallStatementType.FOREACH_CYCLE);
+                }else if(statement instanceof ifCondition){
+                    statementTypeMap.put(statement, EallStatementType.IF_CONDITION);
+                }else if(statement instanceof procedureDefinition){
+                    statementTypeMap.put(statement, EallStatementType.PROCEDURE_DEFINITION);
+                }else if(statement instanceof repeatUntilCycle){
+                    statementTypeMap.put(statement, EallStatementType.REPEAT_UNTIL_CYCLE);
+                }else if(statement instanceof whileCycle){
+                    statementTypeMap.put(statement, EallStatementType.WHILE_CYCLE);
+                }
+            }
+        }
 
         return statementTypeMap;
     }
